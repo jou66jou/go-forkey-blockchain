891d62f9c46777d0ec468acf3a78b96c7df0272e
JaseHuangT480 <jou66jou@gmail.com>
2019-07-23 11:35:04 +0800

    noting

--- p2p/event.go
+++
@@ -1,9 +0,0 @@
-package p2p
-
-const (
-	ADD_PEER            = 0  // 新增節點
-	QUERY_LATEST        = 1  // 取得最後一個區塊
-	QUERY_ALL           = 2  // 取得全部區塊
-	RESPONSE_BLOCKCHAIN = 3  // 回應區塊鏈
-	ERROR               = 10 // 錯誤訊息
-)
